6abf9200cb68ca60b91034a1efb8255076036310
Sleepk <julio.cesar.vgs@gmail.com>
2019-06-20 17:52:03 -0300

    Capitulo 5 - Aula 2 - Instalando TypeScript Definitions

--- alurabank/app/js/views/View.js
+++ alurabank/app/js/views/View.js
@@ -1,9 +1,9 @@
 class View {
     // aonde vamos passar por parametro os itens para carregar o seletor.
     constructor(seletor) {
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
     update(model) {
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 }
--- alurabank/app/ts/views/View.ts
+++ alurabank/app/ts/views/View.ts
@@ -1,12 +1,12 @@
 abstract class View<T> {
-    protected _elemento: Element;
+    protected _elemento: JQuery;
     // aonde vamos passar por parametro os itens para carregar o seletor.
     constructor(seletor: string) {
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
 
     update(model: T): void {
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 
     abstract template(model: T): string ;
