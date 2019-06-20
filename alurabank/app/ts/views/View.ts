680b1f8d5dc9a5f908fd97251f5a7967f2db2a6b
Sleepk <julio.cesar.vgs@gmail.com>
2019-06-20 16:59:05 -0300

    Capitulo 4 - Aula 3 - Lidando com tipos genéricos

--- alurabank/app/ts/views/View.ts
+++ alurabank/app/ts/views/View.ts
@@ -1,8 +1,15 @@
-class View{
+class View<T> {
     protected _elemento: Element;
     // aonde vamos passar por parametro os itens para carregar o seletor.
     constructor(seletor: string) {
         this._elemento = document.querySelector(seletor);
     }
 
+    update(model: T): void {
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    template(model: T): string {
+        throw new Error("Voce deve implementar o metodo template");
+    }
 }
