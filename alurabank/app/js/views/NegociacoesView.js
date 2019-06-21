ebf6add3c4cefa033bc45595936abe22fb6fa5eb
Sleepk <julio.cesar.vgs@gmail.com>
2019-06-21 10:59:42 -0300

    Curso 2 typescript - Capitulo 1 - Aula 4 - Servidor Local

--- alurabank/app/js/views/NegociacoesView.js
+++ alurabank/app/js/views/NegociacoesView.js
@@ -1,7 +1,17 @@
-import { View } from "./View";
-export class NegociacoesView extends View {
-    template(model) {
-        return `
+System.register(["./View"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var View_1, NegociacoesView;
+    return {
+        setters: [
+            function (View_1_1) {
+                View_1 = View_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacoesView = class NegociacoesView extends View_1.View {
+                template(model) {
+                    return `
         <table class="table table-hover table-bordered">
             <thead>
                 <tr>
@@ -13,7 +23,7 @@ export class NegociacoesView extends View {
             </thead>
             <tbody>
             ${model.paraArray().map(negociacao => {
-            return `
+                        return `
                     <tr>
                     <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
                         <td>${negociacao.quantidade}</td>
@@ -21,12 +31,16 @@ export class NegociacoesView extends View {
                         <td>${negociacao.volume}</td>
                     </tr>
                 `;
-        }).join('')}
+                    }).join('')}
             </tbody>
             
             <tfoot>
             </tfoot>
         </table>
         `;
-    }
-}
+                }
+            };
+            exports_1("NegociacoesView", NegociacoesView);
+        }
+    };
+});
